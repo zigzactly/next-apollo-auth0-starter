874dfad895c4803d96061145792342a4f2fc5d82
ian <ian@embreo.co>
2018-04-23 09:58:34 +0800

    Added apollo client and quick exmaple on index.js page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,34 @@
-export default () => <div>next-apollo-auth0-starter</div>
+import withApollo from '../lib/withApollo'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+
+const Index = props => {
+    return (
+        <Query query={query}>
+            {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>
+                if (error) return <p>Error...</p>
+
+                return data.allUsers.map((val, i) => (
+                    <div key = {i}>
+                        <p>{`${val.id} : ${val.email}`}</p>
+                    </div>
+                ))
+            }}
+        </Query>
+    )
+}
+
+
+const query = gql`
+    query {
+        allUsers {
+            id
+            email
+        }
+    }
+`
+
+export default withApollo(props => (
+    <Index />
+))
